803b135b874fa639e96a4fe948ed009824a63ea6
pit-rella <serezha.sliva@gmail.com>
2026-02-03 00:17:04 +0200

    feat(router): include `user_router` with `/user` prefix in API configuration

--- config/router.py
+++ config/router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 
 from config.settings import Settings
 from endpoints import (
-    main_router, auth_router,
+    main_router, auth_router, user_router,
 )
 
 settings = Settings.load()
@@ -22,6 +22,7 @@ def initialize_routers() -> APIRouter:
     """
     main_api_router = APIRouter(prefix=f'/api/{settings.API_VERSION}')
     main_api_router.include_router(main_router, prefix='/health', tags=['main'])
+    main_api_router.include_router(user_router, prefix='/user')
     main_api_router.include_router(auth_router, prefix='/auth', tags=['auth'])
 
 
