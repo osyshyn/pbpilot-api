f807cd0ed2f831edba7a86f7186440c798d7a61d
pit-rella <serezha.sliva@gmail.com>
2026-02-03 00:16:52 +0200

    feat(schemas): add `UserResponseSchema` for user data serialization and update schema exports

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,10 +1,12 @@
 from .main import HealthCheckResponseSchema
 from .auth import SignUpRequestSchema, SignUpResponseSchema
 from .token import TokenResponseSchemas, RefreshTokenRequestSchema
+from .user import UserResponseSchema
 __all__ = [
     'HealthCheckResponseSchema',
     'SignUpRequestSchema',
     'SignUpResponseSchema',
     'TokenResponseSchemas',
     'RefreshTokenRequestSchema',
+    'UserResponseSchema',
 ]
---
+++ schemas/user.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+from models.user import UserRoleEnum
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    name: str
+    surname: str
+    email: str
+    role: UserRoleEnum
+    is_active: bool
+    phone_number: Annotated[
+        str | None,
+        Field(
+            default=None,
+            alias='phone_number',
+            description='Phone number of the user',
+        ),
+    ] = None
