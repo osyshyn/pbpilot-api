4b26f681a6bf496d083696083fa319a4b747423b
pit-rella <serezha.sliva@gmail.com>
2026-02-02 23:54:09 +0200

    feat(services, endpoints): implement `AuthService` and add user authentication routes

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -4,8 +4,13 @@ from fastapi import APIRouter, Depends
 
 from config.settings import Settings
 from core import get_service
-from schemas import SignUpResponseSchema, SignUpRequestSchema
-from services import UserService
+from fastapi.security import OAuth2PasswordRequestForm
+from schemas import (
+    SignUpResponseSchema,
+    SignUpRequestSchema,
+    TokenResponseSchemas
+)
+from services import UserService, AuthService
 
 auth_router = APIRouter()
 settings = Settings.load()
@@ -31,3 +36,34 @@ async def signup_user(
 
     """
     return await service.create_new_user(user_data=user_data)
+
+
+@auth_router.post(
+    path='/login',
+    response_model=TokenResponseSchemas,
+    summary='User login',
+    description=(
+            'Authenticate user with email and password to get access to tokens.'
+    ),
+)
+async def login_user(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        service: Annotated[AuthService, Depends(get_service(AuthService))],
+) -> TokenResponseSchemas:
+    """Authenticate user and return access and refresh tokens.
+
+    Args:
+        form_data (OAuth2PasswordRequestForm): Username and password.
+        service (AuthService): Auth service dependency.
+
+    Returns:
+        TokenSchemas: Access and refresh tokens with type.
+
+    """
+    user = await service.auth_user(
+        email=form_data.username,
+        password=form_data.password,
+    )
+    return await service.create_token(
+        author_id=user.id, user_role=user.role
+    )
--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,4 @@
 from .user import UserService
+from .auth import AuthService
+
+__all__ = ['UserService','AuthService']
