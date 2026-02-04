a289156cbe90e1fc4a456ed7df0e284aebffbbcc
pit-rella <serezha.sliva@gmail.com>
2026-02-04 18:12:40 +0200

    feat(schemas): add `BaseModelSchema` with `from_attributes` config and update exports

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,11 @@
 from .dependencies import get_service
 from .handlers import exception_handler, timing_handler
 from .service import BaseService
-
+from .schemas import BaseModelSchema
 __all__ = [
     'BaseService',
     'exception_handler',
     'get_service',
     'timing_handler',
+    'BaseModelSchema',
 ]
