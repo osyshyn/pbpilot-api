00d9d56aa45ee05552270235b202f30f4e2b7e52
pit-rella <serezha.sliva@gmail.com>
2026-02-09 17:13:25 +0200

    feat(models, migrations): add `Client` model with corresponding schema and migration

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .blacklist_token import BlacklistToken
 from .user import User
 from .pricing_plan import PricingPlan
+from .client import Client
 
-__all__ = ['BlacklistToken', 'User', 'PricingPlan']
+__all__ = ['BlacklistToken', 'User', 'PricingPlan','Client']
---
+++ models/client.py
@@ -0,0 +1,39 @@
+from enum import StrEnum
+
+from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import BaseIdMixin, BaseTimeStampMixin, SoftDelete
+
+
+class Client(BaseIdMixin, BaseTimeStampMixin, SoftDelete):
+    __tablename__ = 'clients'
+    name: Mapped[str] = mapped_column(
+        String(32),
+        nullable=False,
+        comment='First name of the client',
+    )
+    surname: Mapped[str] = mapped_column(
+        String(32),
+        nullable=False,
+        comment='Last name of the client',
+    )
+    email: Mapped[str] = mapped_column(
+        String(254),
+        nullable=False,
+        unique=True,
+        comment='Unique email address for login',
+    )
+    phone_number: Mapped[str | None] = mapped_column(
+        String(16), nullable=True, comment='Phone number of the user'
+    )
+
+    business_address: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    @property
+    def full_name(self) -> str:
+        """Return the full name of the client."""
+        return f'{self.name} {self.surname}'
