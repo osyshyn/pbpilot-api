28788366fd825a59609cf127a50f9de7627b7895
pit-rella <serezha.sliva@gmail.com>
2026-02-09 21:04:07 +0200

    feat(admin): add admin views for `User` and `Client` models

---
+++ admins/__init__.py
@@ -0,0 +1,4 @@
+from .user import UserAdmin
+from .client import ClientAdmin
+
+__all__ = ['UserAdmin', 'ClientAdmin']
---
+++ admins/client.py
@@ -0,0 +1,22 @@
+from typing import ClassVar
+
+from sqladmin import ModelView
+from models import Client
+from core.admin import TimestampAdminMixin
+
+class ClientAdmin(TimestampAdminMixin, ModelView, model=Client):
+
+    column_list: ClassVar = [
+        Client.id,
+        Client.name,
+        Client.surname,
+        Client.email,
+        Client.phone_number,
+        Client.business_address,
+    ]
+    form_excluded_columns: ClassVar = [Client.created_at, Client.updated_at]
+
+    form_args: ClassVar = {
+        **TimestampAdminMixin.form_args,
+        'balance': {'render_kw': {'readonly': True, 'disabled': True}},
+    }
