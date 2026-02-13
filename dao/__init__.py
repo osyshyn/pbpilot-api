d7c7287e27e6e1adb4a3dc9ad6f28c9cc7040a33
pit-rella <serezha.sliva@gmail.com>
2026-02-13 19:17:22 +0200

    feat(dao): add `CompanyDAO` with boilerplate and update `__all__` in `dao.__init__.py`

--- dao/__init__.py
+++ dao/__init__.py
@@ -3,11 +3,12 @@ from .client import ClientDAO
 from .pricing_plan import PricingPlanDAO
 from .project import ProjectDAO
 from .user import UserDAO
-
+from .company import CompanyDAO
 __all__ = [
     'BlacklistTokenDAO',
     'ClientDAO',
     'PricingPlanDAO',
     'ProjectDAO',
     'UserDAO',
+    'CompanyDAO',
 ]
---
+++ dao/company.py
@@ -0,0 +1,12 @@
+from core.dao import BaseDAO
+from models import Company
+
+
+class CompanyDAO(BaseDAO):
+    """DAO for Client model."""
+
+    async def create(
+        self,
+        *,
+    ) -> Company:
+        pass
