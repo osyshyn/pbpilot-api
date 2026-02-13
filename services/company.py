60f9dd3647eac93c5409a8422a43a63dffa93b2d
pit-rella <serezha.sliva@gmail.com>
2026-02-13 19:27:03 +0200

    feat(services): implement `create_company` method in `CompanyService` with error handling and database integration

--- services/company.py
+++ services/company.py
@@ -1,9 +1,10 @@
 import logging
-
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import BaseService
 from dao import CompanyDAO
+from exceptions import CompanyAlreadyExistsNotFoundException
 from models import Company
 from schemas import CreateClientRequestSchema
 
@@ -23,4 +24,17 @@ class CompanyService(BaseService):
     async def create_company(
             self, company_data: CreateClientRequestSchema
     ) -> Company:
-        pass
+        try:
+            company = await self._company_dao.create(
+                company_name=company_data.company_name,
+                phone_number=company_data.phone_number,
+                address=company_data.address,
+                timezone=company_data.timezone,
+                logo_key=company_data.logo_key,
+                tax_state=company_data.tax_state,
+                tax_percentage=company_data.tax_percentage,
+            )
+        except IntegrityError:
+            raise CompanyAlreadyExistsNotFoundException from None
+        await self._session.commit()
+        return company
