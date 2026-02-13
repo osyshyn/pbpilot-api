d48666f700469c330c15abce69de21b11c5b7e17
pit-rella <serezha.sliva@gmail.com>
2026-02-13 19:23:56 +0200

    feat(dao): implement `create` method in `CompanyDAO` to handle company creation logic

--- dao/company.py
+++ dao/company.py
@@ -6,6 +6,26 @@ class CompanyDAO(BaseDAO):
     """DAO for Client model."""
 
     async def create(
-        self,
+            self,
+            *,
+            company_name: str,
+            phone_number: str,
+            address: str,
+            timezone: str,
+            logo_key: str,
+            tax_state: str,
+            tax_percentage: float,
     ) -> Company:
-        pass
+        company = Company(
+            company_name=company_name,
+            phone_number=phone_number,
+            address=address,
+            timezone=timezone,
+            logo_key=logo_key,
+            tax_state=tax_state,
+            tax_percentage=tax_percentage,
+        )
+        self._session.add(company)
+        await self._session.flush()
+        await self._session.refresh(company)
+        return company
