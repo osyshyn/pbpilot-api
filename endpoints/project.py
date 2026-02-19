afb4e9cc2b0cebca86b27183a83887ac9232e4d1
pit-rella <serezha.sliva@gmail.com>
2026-02-19 18:53:33 +0200

    feat(projects): added `/search` endpoint to query projects by name and updated query parameter handling

--- endpoints/project.py
+++ endpoints/project.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 
 from core import get_service
 from core.pagination import PaginatedResponse, PaginationParams
@@ -20,14 +20,32 @@ project_router = APIRouter()
 
 
 @project_router.get(
+    path='/search',
+    description='Search projects by name',
+)
+async def search_projects(
+        admin_user: Annotated[User, Depends(get_admin_user_from_token)],
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
+        project_name: str | None = Query(
+            None,
+            description='Project name for search',
+            examples=['test project'],
+        ),
+) -> ProjectResponseSchema:
+    pass
+
+
+@project_router.get(
     path='/dashboard',
     summary='Get dashboard data',
 )
 async def get_project_dashboard(
-    admin_user: Annotated[User, Depends(get_admin_user_from_token)],
-    project_service: Annotated[
-        ProjectService, Depends(get_service(ProjectService))
-    ],
+        admin_user: Annotated[User, Depends(get_admin_user_from_token)],
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
 ) -> ProjectDashboardResponseSchema:
     dashboard_dto = await project_service.get_projects_dashboard(admin_user.id)
     return ProjectDashboardResponseSchema.model_validate(dashboard_dto)
@@ -39,10 +57,10 @@ async def get_project_dashboard(
     dependencies=[Depends(get_admin_user_from_token)],
 )
 async def create_project(
-    project_data: CreateProjectRequestSchema,
-    project_service: Annotated[
-        ProjectService, Depends(get_service(ProjectService))
-    ],
+        project_data: CreateProjectRequestSchema,
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
 ) -> ProjectResponseSchema:
     project = await project_service.create_project(data=project_data)
     return ProjectResponseSchema.model_validate(project)
@@ -54,10 +72,10 @@ async def create_project(
     dependencies=[Depends(get_admin_user_from_token)],
 )
 async def get_project_by_id(
-    project_id: int,
-    project_service: Annotated[
-        ProjectService, Depends(get_service(ProjectService))
-    ],
+        project_id: int,
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
 ) -> ProjectResponseSchema:
     project = await project_service.get_project_by_id(project_id=project_id)
     return ProjectResponseSchema.model_validate(project)
@@ -69,10 +87,10 @@ async def get_project_by_id(
     dependencies=[Depends(get_admin_user_from_token)],
 )
 async def delete_project_by_id(
-    project_id: int,
-    project_service: Annotated[
-        ProjectService, Depends(get_service(ProjectService))
-    ],
+        project_id: int,
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
 ) -> ProjectResponseSchema:
     project = await project_service.delete_by_id(project_id=project_id)
     return ProjectResponseSchema.model_validate(project)
@@ -84,10 +102,10 @@ async def delete_project_by_id(
     dependencies=[Depends(get_admin_user_from_token)],
 )
 async def get_all_projects(
-    pagination: Annotated[PaginationParams, Depends()],
-    project_service: Annotated[
-        ProjectService, Depends(get_service(ProjectService))
-    ],
+        pagination: Annotated[PaginationParams, Depends()],
+        project_service: Annotated[
+            ProjectService, Depends(get_service(ProjectService))
+        ],
 ) -> PaginatedResponse[ProjectResponseSchema]:
     items, total = await project_service.get_all_projects(
         page=pagination.page,
