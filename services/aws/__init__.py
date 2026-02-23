76c1ca388cb53a62ecd4d61d5e214afe8986078d
pit-rella <serezha.sliva@gmail.com>
2026-02-24 00:23:51 +0200

    ref(aws): replace `UploadFile` with `FileUploadService` in module exports

--- services/aws/__init__.py
+++ services/aws/__init__.py
@@ -1,5 +1,5 @@
 from .actions import S3Actions
 from .base import AWSActions
 from .mixins import FileActionMixin
-from .upload import UploadFile
-__all__ = ['AWSActions', 'FileActionMixin', 'S3Actions', 'UploadFile']
+from .upload import FileUploadService
+__all__ = ['AWSActions', 'FileActionMixin', 'S3Actions', 'FileUploadService']
