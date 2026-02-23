ca3a0ab214777cb62c60fcd4fcb534d5bae236ee
pit-rella <serezha.sliva@gmail.com>
2026-02-24 00:14:36 +0200

    refactor(exceptions): initialize domain-level exceptions and export them

---
+++ exceptions/domain/__init__.py
@@ -0,0 +1,17 @@
+from .file import (
+    BaseDomainFileException,
+    EmptyFileNameException,
+    EmptyFileException,
+    UnknownFiletypeException,
+    IncorrectFileSizeException,
+    FileUploadException,
+)
+
+__all__ = [
+    'BaseDomainFileException',
+    'EmptyFileNameException',
+    'EmptyFileException',
+    'UnknownFiletypeException',
+    'IncorrectFileSizeException',
+    'FileUploadException',
+]
