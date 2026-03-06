dfbcee130479637a57371fa0206cff475bd02ccb
Tarasgr7 <tarasgrickyv@gmail.com>
2026-03-06 20:58:28 +0200

    fix(settings): update environment file handling for development mode
    Refactor the environment file selection logic to ensure the correct .env file is used for the 'dev' environment. This change improves clarity and consistency in environment configuration.

--- config/settings.py
+++ config/settings.py
@@ -14,14 +14,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).resolve().parent.parent
 """Base directory of the project."""
 
+
 env_mode = os.getenv('ENV', 'local')
 """Current environment mode (dev, prod, etc.)."""
 
 match env_mode:
+    case 'dev':
+        env_file = BASE_DIR / '.env'
     case 'prod':
         env_file = BASE_DIR / '.env.prod'
-    case 'dev':
-        env_file = BASE_DIR / '.env.dev'
     case 'local':
         env_file = BASE_DIR / '.env.local'
 """Path to environment-specific .env file."""
